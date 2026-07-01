3b8a6b98f61e52bdfe51df5e07960a7c789ec0c1
Thales Gregorio <137453849+thales-gsd@users.noreply.github.com>
2026-07-01 12:30:29 -0300

    build android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zul.code',
+  appName: 'ZulCode',
+  webDir: 'out'
+};
+
+export default config;
